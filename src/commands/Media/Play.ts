9b8fbebcc86454241e1e6babeca9bb663a053725
minato1220 <100998375+minato1220@users.noreply.github.com>
2022-05-22 18:24:47 +0530

    Update Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: videos[0].title.substr(0, 30),
-                        body: `🧨 DeXxY 🧨`,
+                        body: `🧨 𝙠𝙪𝙧𝙪𝙢𝙞 🧨`,
                         mediaType: 2,
                         thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`,
                         mediaUrl: audio.url
