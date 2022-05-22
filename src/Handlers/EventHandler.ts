171b1a504355f0fdf23daa57c9a772fb48716df4
minato1220 <100998375+minato1220@users.noreply.github.com>
2022-05-22 18:36:26 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -25,7 +25,7 @@ export default class EventHandler {
 					: event.action === "remove"
 					? `Goodbye *@${
 							event.participants[0].split("@")[0]
-					  }* 👋🏻, we're probably not gonna miss you.`
+					  }* 👋🏻, Now whom will i make fun of? 😞.`
 					: `Ara Ara, looks like *@${
 							event.participants[0].split("@")[0]
 					  }* got ${this.client.util.capitalize(event.action)}d${
